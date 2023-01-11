d2fe221b01ab85ae5526daae1d82a990b51ac6b5
Gregory <vasiliev.gregory@gmail.com>
2023-01-11 12:13:51 +0400

    install docker

--- setup/setup_on_ubuntu.sh
+++ setup/setup_on_ubuntu.sh
@@ -7,6 +7,9 @@ sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo
 sudo add-apt-repository universe
 sudo apt -y install tmux neovim gnome-tweaks git zsh telegram-desktop gnome-shell-extension-prefs
 
+git config --global user.email "vasiliev.gregory@gmail.com"
+git config --global user.name "Gregory"
+
 echo 'Keyboard & Mouse > Keyboard > Additional Layout Options > Caps Lock behavior'
 sleep 5
 gnome-tweaks
@@ -25,3 +28,28 @@ echo log out and bach in for zsh to bacome default shell
 
 git clone https://github.com/sindresorhus/pure.git "$HOME/dotfiles/zsh/pure"
 git clone https://github.com/jeffreytse/zsh-vi-mode.git $HOME/dotfiles/zsh/zsh-vi-mode
+
+
+# https://docs.docker.com/engine/install/ubuntu/
+echo install docker
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo docker run hello-world
