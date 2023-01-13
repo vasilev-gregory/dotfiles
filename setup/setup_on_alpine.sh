831bb0c773a604cd75cd1b554e600c8a1d61d385
Gregory <vasiliev.gregory@gmail.com>
2023-01-13 10:30:28 +0400

    alpine setup

--- setup/setup_on_alpine.sh
+++ setup/setup_on_alpine.sh
@@ -1,8 +1,11 @@
 apk --no-cache update
-apk --no-cache add neovim ripgrep
+apk --no-cache add neovim ripgrep python3 python3-pip
+
+pip3 install --user pynvim
 
 mkdir -p ~/.config/nvim
 ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -s ~/dotfiles/bash/bashrc ~/.bashrc
 
 nvim +PlugInstall +qall
+nvim +UpdateRemotePlugins +qall
