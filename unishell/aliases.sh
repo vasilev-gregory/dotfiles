e57cc42b296d10880562b623eb94cb216e4d65bf
Gregory <vasiliev.gregory@gmail.com>
2023-08-09 09:28:57 +0400

    conky less info about top proceses cpu util back and alignment to middle

--- unishell/aliases.sh
+++ unishell/aliases.sh
@@ -1,2 +1,3 @@
 which fdfind &>/dev/null && alias fd=fdfind
 alias update='sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y'
+alias t=task
