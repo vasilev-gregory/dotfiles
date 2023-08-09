0be9e73a0b19b0b078ce407e7c53e7b637089447
Gregory <vasiliev.gregory@gmail.com>
2023-08-09 17:44:14 +0400

    taskrc and conky to show pending active

--- unishell/aliases.sh
+++ unishell/aliases.sh
@@ -1,3 +1,4 @@
 which fdfind &>/dev/null && alias fd=fdfind
 alias update='sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y'
 alias t=task
+alias ta=task add
